cac453cc67c02da3bb293e21af1a67bf9bb472d7
umairqidwai1 <134027101+umairqidwai1@users.noreply.github.com>
2024-10-04 20:28:28 -0400

    Update startAPP.sh

--- startAPP.sh
+++ startAPP.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /home/pi/Desktop/Athan-speaker-using-Raspberry-pi
-git pull origin main
 cd /home/pi/Desktop
 source env/bin/activate
 cd /home/pi/Desktop/Athan-speaker-using-Raspberry-pi
