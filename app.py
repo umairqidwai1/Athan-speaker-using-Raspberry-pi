d51a235e0358e51a3374f4b5389e4772e1d664b6
umairqidwai1 <134027101+umairqidwai1@users.noreply.github.com>
2024-10-17 12:28:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -409,4 +409,5 @@ def remove_athan():
 start_background_thread()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+
