7aabf6b4a24e20cb0a409890999f3d5e5c3a7e65
umairqidwai1 <134027101+umairqidwai1@users.noreply.github.com>
2024-10-25 17:41:38 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cd /home/pi/Desktop
-source env/bin/activate
 cd /home/pi/Desktop/Athan-speaker-using-Raspberry-pi
-sudo env "PATH=$PATH" python monitor.py
+sudo /home/pi/Desktop/env/bin/python monitor.py >> /home/pi/Desktop/monitor.log 2>&1 &
