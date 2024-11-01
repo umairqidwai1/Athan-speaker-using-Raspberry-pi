f5ee20271f74279774aa77c75cc448671e2c65ac
umairqidwai1 <134027101+umairqidwai1@users.noreply.github.com>
2024-11-01 07:53:08 -0400

    Update startAPP.sh

--- startAPP.sh
+++ startAPP.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/pi/Desktop/Athan-speaker-using-Raspberry-pi
-sudo /home/pi/Desktop/env/bin/python app.py >> /home/pi/Desktop/athan-app.log 2>&1 &
+sudo /home/pi/Desktop/env/bin/python app.py
