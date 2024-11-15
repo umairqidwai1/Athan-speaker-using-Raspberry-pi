c57d7986ea4b66d7f88b0f82b3835ffefe880579
umairqidwai1 <134027101+umairqidwai1@users.noreply.github.com>
2024-11-14 22:47:03 -0500

    Update startAPP.sh

--- startAPP.sh
+++ startAPP.sh
@@ -2,5 +2,12 @@
 cd /home/pi/Desktop
 source env/bin/activate
 cd /home/pi/Desktop/Athan-speaker-using-Raspberry-pi
-pip3 install -r requirements.txt
+pip install flask_socketio
+pip install schedule
+pip install yt-dlp
+pip install pygame
+pip install psutil
+pip install flask
+pip install pydub
+pip install evdev
 sudo env "PATH=$PATH" python app.py
