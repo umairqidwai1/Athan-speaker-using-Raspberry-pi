5f23647d4864c442a9b6e0b7bb8cdd0b4bd374eb
umairqidwai1 <134027101+umairqidwai1@users.noreply.github.com>
2025-01-08 09:21:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -303,7 +303,7 @@ def main_loop():
             play_regular_athan()
         elif current_time == ISHA:
             play_regular_athan()
-        elif current_time == '08:30':
+        elif current_time == '09:30':
             play_regular_athan()
 
         # Sleep for a second before checking again
