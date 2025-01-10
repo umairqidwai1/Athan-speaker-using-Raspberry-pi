3622165f6fdfb94f4a04fff10c98efec6b00e69d
umairqidwai1 <134027101+umairqidwai1@users.noreply.github.com>
2025-01-10 16:40:16 -0500

    Update startAPP.sh

--- startAPP.sh
+++ startAPP.sh
@@ -2,4 +2,4 @@
 cd /home/pi/Desktop/Athan-speaker-using-Raspberry-pi
 source /home/pi/Desktop/env/bin/activate
 pip install -r requirements.txt
-sudo /home/pi/Desktop/env/bin/python app.py
+/home/pi/Desktop/env/bin/python app.py
