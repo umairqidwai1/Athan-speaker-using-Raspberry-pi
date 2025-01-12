cdf93e92897ea5bc11645b744a9372c77c90638d
umairqidwai1 <134027101+umairqidwai1@users.noreply.github.com>
2025-01-12 18:51:22 -0500

    Update auto_pull.sh

--- auto_pull.sh
+++ auto_pull.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /home/pi/Desktop/Athan-speaker-using-Raspberry-pi
-git fetch origin Test
+git fetch origin test3
 LOCAL=$(git rev-parse HEAD)
 REMOTE=$(git rev-parse origin/main)
 
 if [ $LOCAL != $REMOTE ]; then
     echo "New changes detected. Pulling latest code..."
-    git pull origin Test
+    git pull origin test3
 else
     echo "No changes."
 fi
